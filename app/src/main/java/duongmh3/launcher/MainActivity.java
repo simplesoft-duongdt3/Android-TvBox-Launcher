8e49e5b548668b698deff94f9b6f88c1687f5610
admin <admin@admins-MacBook-2.local>
2017-10-10 22:59:06 +0700

    add empty activity

--- app/src/main/java/duongmh3/launcher/MainActivity.java
+++ app/src/main/java/duongmh3/launcher/MainActivity.java
@@ -1,41 +1,92 @@
 package duongmh3.launcher;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.AppCompatImageView;
+import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.daimajia.slider.library.SliderLayout;
 import com.daimajia.slider.library.SliderTypes.BaseSliderView;
 import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.zhy.android.percent.support.PercentRelativeLayout;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import duongmh3.launcher.common.list.RecyclerItems;
 import duongmh3.launcher.common.task.TaskManager;
 import duongmh3.launcher.func.listapp.AppInfoViewModel;
-import duongmh3.launcher.func.listapp.AppViewModelBinder;
 import duongmh3.launcher.func.listapp.LoadAppUseCase;
-import info.awesomedevelopment.tvgrid.library.TVGridView;
 import me.drakeet.multitype.MultiTypeAdapter;
 
 public class MainActivity extends AppCompatActivity {
     @NonNull
     private final TaskManager taskManager = new TaskManager();
+    @BindView(R.id.vgHeader)
+    RelativeLayout vgHeader;
+    @BindView(R.id.cv1)
+    CardView cv1;
+    @BindView(R.id.cv2)
+    CardView cv2;
+    @BindView(R.id.cv3)
+    CardView cv3;
+    @BindView(R.id.cv4)
+    CardView cv4;
+    @BindView(R.id.cv5)
+    CardView cv5;
+    @BindView(R.id.cv6)
+    CardView cv6;
+    @BindView(R.id.cv7)
+    CardView cv7;
+    @BindView(R.id.cv8)
+    CardView cv8;
+    @BindView(R.id.slider)
+    SliderLayout slider;
+    @BindView(R.id.vgFixApp)
+    PercentRelativeLayout vgFixApp;
+    @BindView(R.id.tvApps)
+    TextView tvApps;
+    @BindView(R.id.cvApp1)
+    CardView cvApp1;
+    @BindView(R.id.cvApp2)
+    CardView cvApp2;
+    @BindView(R.id.cvApp3)
+    CardView cvApp3;
+    @BindView(R.id.cvApp4)
+    CardView cvApp4;
+    @BindView(R.id.cvApp5)
+    CardView cvApp5;
+    @BindView(R.id.cvApp6)
+    CardView cvApp6;
+    @BindView(R.id.cvApp7)
+    CardView cvApp7;
+    @BindView(R.id.cvApp8)
+    CardView cvApp8;
+    @BindView(R.id.cvApp9)
+    CardView cvApp9;
+    @BindView(R.id.cvApp10)
+    CardView cvApp10;
+
+
     //private TVGridView rvApp;
     private RecyclerItems<AppInfoViewModel> items = new RecyclerItems<>();
     @NonNull
     private final LoadAppUseCase loadAppUseCase = new LoadAppUseCase(taskManager);
     @NonNull
-    private final MultiTypeAdapter multiTypeAdapter = new MultiTypeAdapter();;
+    private final MultiTypeAdapter multiTypeAdapter = new MultiTypeAdapter();
+    ;
     private SliderLayout sliderLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         initView();
         initData();
     }
